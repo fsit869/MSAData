e04e825ae2c8859c0a831e551d326960825986cd
fsit869 <62918263+fsit869@users.noreply.github.com>
2021-12-05 16:21:22 +1300

    Update StuProResponse.java

--- src/main/java/com/example/databasetesting/jpa/dto/StuProResponse.java
+++ src/main/java/com/example/databasetesting/jpa/dto/StuProResponse.java
@@ -13,4 +13,11 @@ import lombok.ToString;
 public class StuProResponse {
     private String firstName;
     private String productName;
+
+    /*
+    Note if you want more variables but some of them optional,
+    YOU MUST create a new constructor for the ones you just want
+    Since remember in repo, we create new instance of this class.
+    Therefore we need appropriate constructors
+     */
 }
